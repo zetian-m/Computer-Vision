abec79d314619edba903080713c5da2e84434832
Zetian Mu <zetian-m@web.de>
2022-08-21 19:52:05 +0200

    Use gauss and sobel filter in main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,70 @@
 import common as CO
+import GaussFilter as GAUSF
+import SobelFilter as SOF
+import matplotlib.pyplot as plt
+import sys
+import logging
+
+#############################################
+#                                           #
+#                 Constants                 #
+#                                           #
+#############################################
+def constant(f):
+    """ Definetion of constants in this Python-Script.
+        If you try to change the constants, it will show error.
+    Args:
+        f (_type_): _description_
+    """
+    def fset(self, value):
+            raise TypeError
+    def fget(self):
+            return f()
+    return property(fget, fset)
+
+class _GAUSSFILCONST(object):
+    """ Constants for using gauss filter
+    Returns:
+        _int_: kernelsize, sigma
+    """
+    @constant
+    def SIGMA():
+        return 3
+    @constant
+    def KERNELSIZE():
+        return 3
+
+class _SOBELFILCONST(object):
+    """ Constants for using sobel filter
+    Returns:
+        _int_: kernelsize
+    """
+    @constant
+    def KERNELSIZE():
+        return 3
+#############################################
+#                                           #
+#              End of Constants             #
+#                                           #
+#############################################    
 
 
-def main():
-    originalImg = CO.loadImage('Picture_Crossing_noise_0_pixelCnt_128_featureCnt_5.bmp')
-    print(originalImg)
-    pass
-
+    
 if __name__ == "__main__":
-    main()
+    GAUSSFCONST = _GAUSSFILCONST()
+    SOFCONST = _SOBELFILCONST()
+
+    originalImg = CO.loadImage('Picture_Crossing_noise_0_pixelCnt_128_featureCnt_5.bmp')
+
+    #* GAUSS filtering
+    gaussKernel, resultImg = GAUSF.gaussFilter(originalImg, GAUSSFCONST.KERNELSIZE, GAUSSFCONST.SIGMA)
+
+    #* SOBEL filtering
+    resultImg = SOF.sobelFilter(originalImg, SOFCONST.KERNELSIZE)
+
+    #* Plotting
+    plt.imshow(resultImg, interpolation='none', cmap='gray')
+    plt.show()
+
+
+
