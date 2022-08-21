b60b25c5695c5f4f0a3e6830b3248976391ee8a8
Zetian Mu <zetian-m@web.de>
2022-08-21 19:51:48 +0200

    Add convolution func

--- src/common.py
+++ src/common.py
@@ -8,6 +8,9 @@
 from PIL import Image
 import cv2
 import os 
+import numpy as np
+import logging
+import matplotlib.pyplot as plt
 
 def fun():
     """ Function documentation example
@@ -15,7 +18,7 @@ def fun():
     pass
 
 def loadImage(imgFileName):
-    
+
     currentPath = os.getcwd()
 
     #* Use \\ instead \
@@ -23,12 +26,41 @@ def loadImage(imgFileName):
 
     print(imagePath)
 
-    original_image = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+    #* Could also use the cv2 function instead
+    original_image = plt.imread(imagePath, plt.gray)
 
     return original_image
 
-def convolution():
-    pass
+def convolution(image, kernel):
+
+    #* Get Size of image and kernel
+    imgSizeX, imgSizeY = image.shape
+    kernelSizeX, kernelSizeY = kernel.shape
+
+    
+    
+    #* Check if both are square
+    if imgSizeX == imgSizeY:
+        imgSize = imgSizeX
+    else:
+        logging.warning("imgSizeX != imgSizeY")
+    if kernelSizeX == kernelSizeY:
+        kernelSize = kernelSizeX
+    else:
+        logging.warning("kernelSizeX != kernelSizeY")
+
+    resultSizeX = imgSize - kernelSize + 1
+    resultSizeY = imgSize - kernelSize + 1
+
+    #* Notice that np.zeros() has different position of X and Y in input
+    resultImage = np.zeros((resultSizeY,resultSizeX))
+
+    for i in range(resultSizeX):
+        for j in range(resultSizeY):
+            resultImage[j][i] = np.sum(image[j:j+kernelSize, i:i+kernelSize] * kernel)
+    
+    return resultImage
 
 if __name__ == "__main__":
+    #* Write the module test function here
     pass
