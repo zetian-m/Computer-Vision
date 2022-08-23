f612c6f2cac6a2c49374a8bf58d5d462fbe2034a
Zetian Mu <zetian-m@web.de>
2022-08-23 16:39:46 +0200

    Update Canny Filter

--- src/CannyFilter.py
+++ src/CannyFilter.py
@@ -4,7 +4,8 @@ import SobelFilter as SOBF
 import matplotlib.pyplot as plt
 import numpy as np
 
-def nearest_quarter(ang):
+def nearestQuarter(ang):
+    # round the angle to a quarter
     return (((round(((ang)/np.pi)/0.5))/4)*(2*np.pi)) % (2*np.pi)
 
 def roundAngle(sobelPixelAngle):
@@ -13,13 +14,77 @@ def roundAngle(sobelPixelAngle):
     pixelAngleRounded = np.zeros(pixelMatrixSize)
     for x in range (pixelMatrixSize[0]):
         for y in range (pixelMatrixSize[1]):
-            print(sobelPixelAngle[x:y])
-            pixelAngleRounded[x:y] = nearest_quarter(nearest_quarter(sobelPixelAngle[x:y]))
-    #for pixelArray, pixelRoundedArray in sobelPixelAngle, pixelAngleRounded:
-    #    for pixel, pixelRounded in pixelArray, pixelRoundedArray:
-            
+            pixelAngleRounded[x][y] = nearestQuarter(sobelPixelAngle[x][y])
 
     return pixelAngleRounded
+
+def setPixelGradientValue(pixelGradientList):
+
+    maxPixelGradient = np.amax(pixelGradientList)
+
+    for index, pixelGradient in np.ndenumerate(pixelGradientList):
+        if pixelGradient != maxPixelGradient:
+            pixelGradient = 0
+        else:
+            continue
+    return pixelGradientList
+
+def nonMaximaSuppression(edgeGradient, pixelAngleRounded):
+    TOP = 0
+    RIGHT = 0.5*np.pi
+    BOTTOM = np.pi
+    LEFT = 1.5*np.pi
+
+    if edgeGradient.shape != pixelAngleRounded.shape:
+        return -1
+
+    gradientSizeX, gradientSizeY = edgeGradient.shape
+
+    newEdgeGradient = np.zeros((gradientSizeX, gradientSizeY))
+
+    #* Because of 3x3 submatrix, the iteration goes only for (size-2)
+    for y in range(gradientSizeY-2):
+        for x in range(gradientSizeX-2):
+            #* get 3x3 submatrix for nonMaximaSuppression
+            subEdgeGradient = edgeGradient[y:y+3, x:x+3]
+            subPixelAngleRounded = pixelAngleRounded[y:y+3, x:x+3]
+
+            #* get the central point value -> angle and gradient
+            centralPixelAngle = subPixelAngleRounded.item((1,1))
+            #centralPixelGradient = subEdgeGradient.item((1,1))
+
+            #* check the angle direction
+            if  centralPixelAngle == TOP or centralPixelAngle == BOTTOM:
+                #* compare the central gradient value with TOP and BOTTOM
+                pixelGradientList = subEdgeGradient[1:2, 0:3]
+
+                pixelGradientList = setPixelGradientValue(pixelGradientList)
+
+                subEdgeGradient[1:2, 0:3] = pixelGradientList
+
+            elif centralPixelAngle == LEFT or centralPixelAngle == RIGHT:
+
+                pixelGradientList = subEdgeGradient[0:3, 1:2]
+
+                pixelGradientList = setPixelGradientValue(pixelGradientList)
+
+                subEdgeGradient[0:3, 1:2] = pixelGradientList
+
+            else:
+                return -1
+            newEdgeGradient[y:y+3, x:x+3] = subEdgeGradient
+            x += 3
+        y += 3    
+            #print(subEdgeGradient.item(1,1))
+        
+    return newEdgeGradient        
+
+    #todo: Take a 3x3 Matrix
+    #todo: check direction TOP, RIGHT, BOTTOM, LEFT
+    #todo: set 0 or keep value
+    pass
+
+
 
 
 def cannyFilter(inputImg, sigma, sobelKernelSize = 3, convMethod = 0):
@@ -33,15 +98,17 @@ def cannyFilter(inputImg, sigma, sobelKernelSize = 3, convMethod = 0):
     Gx, Gy, EdgeGradient = SOBF.sobelFilter(imgGaussFiltered, sobelKernelSize, convMethod)
 
     #* Step 2.2: Calculate Angle of each pixel 
-    #* (rounded to one of four angles representing vertical, horizontal and two diagonal directions)
+    pixelAngle = np.arctan2(Gy, Gx)
+    print(pixelAngle)
+    
+    #* Step 2.3: Round all angles to a quater of a circle (0, 0.5*pi, pi, 1,5*pi)
+    pixelAngleRounded = roundAngle(pixelAngle)
 
-    sobelPixelAngle = np.arctan2(Gy, Gx)
-    print(sobelPixelAngle)
-    #sobelPixelAngle = sobelPixelAngle.astype(np.uint8)
+    #* Step 3: Application of Non-maxima suppression
+    EdgeGradient = nonMaximaSuppression(EdgeGradient, pixelAngleRounded)
 
-    sobelPixelAngleRounded = roundAngle(sobelPixelAngle)
-    sobelPixelAngleRounded = sobelPixelAngleRounded.astype(np.uint8)
-    plt.imshow(sobelPixelAngleRounded, interpolation='none', cmap='gray')
+    #sobelPixelAngleRounded = sobelPixelAngleRounded.astype(np.uint8)
+    plt.imshow(EdgeGradient, interpolation='none', cmap='gray')
     plt.show()
 
 
