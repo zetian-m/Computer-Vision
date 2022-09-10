bae6a0e5219c85c8b0e85120952db0eef44df5af
Zetian Mu <zetian-m@web.de>
2022-09-10 18:19:06 +0200

    update imageLighter

--- src/PointOperator.py
+++ src/PointOperator.py
@@ -1,15 +1,16 @@
 import cv2 as cv
 import numpy as np
 def imageLighter(image, add_light):
-    for x in range(image.shape[0]):
-        for y in range(image.shape[1]):
-            pixel_hell_temp = image[y,x]
-            pixel_hell_temp += add_light
-            if pixel_hell_temp > 255:
-                pixel_hell_temp = 255
-            image[y,x] = pixel_hell_temp
+    newImg = image
+    for r in range(image.shape[0]):
+        for c in range(image.shape[1]):
+            pixelValue = image[r,c]
+            pixelValue += add_light
+            if pixelValue > 255:
+                pixelValue = 255
+            newImg[r,c] = pixelValue
 
-    return image
+    return newImg
 
 def imageDarker(image, darker):
     for x in range(image.shape[0]):
