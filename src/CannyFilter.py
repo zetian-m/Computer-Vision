02c06170b61355800150e82e2787b9f9b5b2fe5d
Zetian Mu <zetian-m@web.de>
2022-09-12 11:48:38 +0200

    change Gausskernel in Canny

--- src/CannyFilter.py
+++ src/CannyFilter.py
@@ -134,7 +134,7 @@ def cannyThreshold(edgeGradient, thUpper, thLower):
     return newEdgeGradient
 
 def cannyFilter(inputImg, sigma, upperThreshold, lowerThreshold, sobelKernelSize = 3, convMethod = 1 ):
-    GAUSKERNELSIZE = 5
+    GAUSKERNELSIZE = 3
 
     #* Step 1: Noise Reduction with gauss filter by 5x5 Kernel
     gaussKernel, imgGaussFiltered = GAUSF.gaussFilter(inputImg, GAUSKERNELSIZE, sigma, convMethod)
